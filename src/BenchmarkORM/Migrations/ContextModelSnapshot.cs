526a62104f79c333f31964b36c69ec7b063883f1
Milovidov Vasiliy <milovidov.vasya@gmail.com>
2019-02-16 22:27:20 +0300

    add migration

---
+++ src/BenchmarkORM/Migrations/ContextModelSnapshot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using BenchmarkORM;
+
+namespace BenchmarkORM.Migrations
+{
+    [DbContext(typeof(Context))]
+    partial class ContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
+                .HasAnnotation("ProductVersion", "1.1.6");
+
+            modelBuilder.Entity("BenchmarkORM.Entity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<double>("DoubleData");
+
+                    b.Property<int>("IntData");
+
+                    b.Property<string>("StringData");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Entities");
+                });
+        }
+    }
+}
